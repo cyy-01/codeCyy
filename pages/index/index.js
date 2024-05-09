243ab84baf814eedfce410bcbede1e10c90a626c
DESKTOP-3JA5T5T\32951 <3295196626@qq.com>
2024-05-09 18:13:21 +0800

    fix；初步功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,35 @@
 Page({
   data: {
-    name: '崔阳勇',
-    age: '22',
-    qq: '3295196626',
-    wechat: '18288402110',
-    gender: '男',
-    education: '本科',
-    school:'东华理工大学长江学院'
+    name: '崔阳勇---潘自怡',
+    allTime: '',
+    photo: ''
+  },
+
+  // 初始化页面
+  onLoad() {
+    this.startTimer();
+  },
+
+  startTimer() {
+    this.updateTime();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  },
+
+  updateTime() {
+    const startDate = new Date('2023-05-20T20:00:00'); 
+    const now = new Date(); 
+    const duration = now - startDate; 
+
+    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((duration % (1000 * 60)) / 1000);
+    const allTime = `${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`;
+
+    this.setData({
+      allTime
+    });
   }
 });
